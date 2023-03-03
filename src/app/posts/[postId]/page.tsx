1851da3dd81e50b7e89c4428bfbe5ca89a5d195b
surajkumar85 <49224173+surajkumar85@users.noreply.github.com>
2023-03-03 22:08:12 +0530

    added api and genrate statice pages

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,6 +1,34 @@
 import styles from "./page.module.css";
 
-export default function Post({ params }: { params: { postId: string } }) {
+export async function generateStaticParams() {
+  const data = await fetch("https://dummyjson.com/posts", {
+    method: "GET",
+  });
+  const posts = await data.json();
+
+  const idArr = posts.posts.map((post: any) => ({
+    postId: post.id.toString(),
+  }));
+  return idArr;
+}
+
+async function getPost(postId: string) {
+  const data = await fetch(`https://dummyjson.com/posts/${postId}`, {
+    method: "GET",
+  });
+  if (!data.ok) throw new Error(`The requested article does not exists`);
+  const post = await data.json();
+  return post;
+}
+
+export default async function Post({ params }: { params: { postId: string } }) {
   const { postId } = params;
-  return <div>This is post {postId}</div>;
+  const post = await getPost(postId);
+  return (
+    <div>
+      <p>User Id : {post.id}</p>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </div>
+  );
 }
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,25 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
-export default function Post() {
-  return <div className={styles.main}>All Post:</div>;
+async function getAllPost() {
+  const data = await fetch(`https://dummyjson.com/posts?limit=150`, {
+    method: "GET",
+  });
+  const posts = await data.json();
+  return posts;
+}
+
+export default async function Post() {
+  const posts = await getAllPost();
+  // console.log(posts);
+  return (
+    <div className={styles.main}>
+      <h1>All titles below</h1>
+      {posts.posts.map((item: { id: string; title: string }) => (
+        <Link key={item.id} href={`/posts/${item.id}`}>
+          {item.title}
+        </Link>
+      ))}
+    </div>
+  );
 }
